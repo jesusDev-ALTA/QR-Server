72a293caef52fa220c8c8054e09f7fdc22436520
Jesús Quiroz Rojas <jesus.quiroz@altabev.com>
2025-09-09 14:59:56 -0500

    new server

---
+++ server.js
@@ -0,0 +1,44 @@
+// serverA.js
+import { Server } from "socket.io";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+console.log("Iniciando Servidor A...");
+io.on("connection", (socket) => {
+	console.log("Cliente conectado a A:", socket.id);
+	socket.emit("welcome", { message: "Bienvenido al servidor A" });
+	socket.on("welcome-response", (data) => {
+		console.log("Respuesta de bienvenida del cliente:", data);
+	});
+
+	// Escuchar mensajes del cliente
+	socket.on("data-from-A", (data) => {
+		console.log("Mensaje recibido del cliente:", data);
+		// Puedes responder si lo deseas
+		socket.emit("respuesta-de-A", { recibido: true, original: data });
+	});
+
+	// cada 5s emite datos
+	socket.on("Last5QrCodes", (data) => {
+		console.log("Last5QrCodes recibido:", data);
+		// data[0] = array de códigos, data[1] = array de fechas
+		const codes = Array.isArray(data[1]) ? data[1] : [];
+		const dates = Array.isArray(data[2]) ? data[2] : [];
+		const result = codes.map((code, idx) => ({
+			id: idx + 1,
+			code,
+			date: dates[idx],
+		}));
+		console.log("Last5QrCodes recibido y organizado:", result);
+		// Enviar al cliente el JSON organizado
+		io.emit("Last5QrCodesWeb", result);
+	});
+});
+app.use(express.static("dist"));
+
+server.listen(5122, "0.0.0.0", () => {
+	console.log("Servidor A escuchando en puerto 5122");
+});
